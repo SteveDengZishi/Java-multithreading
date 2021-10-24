fab5231e929336d4c6c02dc87c6f661eef0ba944
Steve Deng <zd475@nyu.edu>
2021-10-23 18:02:36 -0600

    consumer update

--- consumerAndProducer/src/main/java/consumer/Consumer.java
+++ consumerAndProducer/src/main/java/consumer/Consumer.java
@@ -1,4 +1,45 @@
 package main.java.consumer;
 
-public class Consumer {
+import java.util.List;
+
+public class Consumer implements Runnable{
+    private final List<Integer> taskQueue;
+
+    public Consumer(List<Integer> queue){
+        this.taskQueue=queue;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            try{
+                consume();
+            } catch(InterruptedException ex){
+                System.out.println("Caught Interrupted Exception while Consuming");
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private void consume() throws InterruptedException{
+        synchronized (taskQueue){
+            while(taskQueue.isEmpty()){
+                //give up the lock on taskQueue
+                System.out.println("Current taskQueue is empty, waiting for new items.");
+                taskQueue.wait();
+            }
+
+            int item = taskQueue.remove(0);
+            System.out.println("Consuming item " + item + " from the top of taskQueue");
+            Thread.sleep(1500);
+            System.out.println("Finished consuming item " + item);
+
+            //release lock and notify other waited thread on the taskQueue when consumed item that reduce max size
+            taskQueue.notifyAll();
+        }
+    }
+
+    List<Integer> getTaskQueue(){
+        return this.taskQueue;
+    }
 }
